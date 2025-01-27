a0c7cd57abd5cd715d14593ab6dfe0811695a98f
Gameoholic <viner.atmic@gmail.com>
2025-01-28 00:10:11 +0200

    feat: add intervals

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
 let piano = document.getElementById('piano');
 let pedalButton = document.getElementById('pedal');
+let interval1Button = document.getElementById('interval-1');
+let interval2Button = document.getElementById('interval-2');
+let interval3Button = document.getElementById('interval-3');
+let interval4Button = document.getElementById('interval-4');
+let interval5Button = document.getElementById('interval-5');
+let interval6Button = document.getElementById('interval-6');
+let interval7Button = document.getElementById('interval-7');
+let interval8Button = document.getElementById('interval-8');
+let interval9Button = document.getElementById('interval-9');
+let interval10Button = document.getElementById('interval-10');
+let interval11Button = document.getElementById('interval-11');
+let interval12Button = document.getElementById('interval-12');
+let interval13Button = document.getElementById('interval-13');
+let interval14Button = document.getElementById('interval-14');
+
 
 let mouseDown = false;
 let pedalTurnedOn = false;
+let interval1TurnedOn = false;
+let interval2TurnedOn = false;
+let interval3TurnedOn = false;
+let interval4TurnedOn = false;
+let interval5TurnedOn = false;
+let interval6TurnedOn = false;
+let interval7TurnedOn = false;
+let interval8TurnedOn = false;
+let interval9TurnedOn = false;
+let interval10TurnedOn = false;
+let interval11TurnedOn = false;
+let interval12TurnedOn = false;
+let interval13TurnedOn = false;
+let interval14TurnedOn = false;
+
 
 
 // Init all notes
@@ -50,9 +80,92 @@ noteNames.forEach(noteName => {
   noteDiv.addEventListener('mouseleave', () => stopNote(note));
 });
 
+// Note utility functions:
 function getNoteFromName(noteName) {
   return notes.find(function (note) { return note.name == noteName;});
 }
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive).
+ * The value is no lower than min (or the next integer greater than min
+ * if min isn't an integer) and no greater than max (or the next integer
+ * lower than max if max isn't an integer).
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+// how many notes from middle C (4) (one side, not including self) to consider when picking note
+function getRandomNote(noteRangeFromMidC) {
+  return notes[notes.indexOf(getNoteFromName('C4')) + getRandomInt(-noteRangeFromMidC, noteRangeFromMidC)];
+}
+
+function playNoteInterval(rootNote, interval) {
+  playNote(notes[notes.indexOf(rootNote) + interval]);
+}
+
+function playNoteIntervalWithDelayAndMode() {
+  const rootNote = getRandomNote(20);
+  playNote(rootNote);
+  playNoteInterval(rootNote, getRandomInterval());
+}
+
+function getRandomInterval() {
+  let availableIntervals = [];
+  if (interval1TurnedOn) {
+    availableIntervals.push(1);
+  }
+  if (interval2TurnedOn) {
+    availableIntervals.push(2);
+  }
+  if (interval3TurnedOn) {
+    availableIntervals.push(3);
+  }
+  if (interval4TurnedOn) {
+    availableIntervals.push(4);
+  }
+  if (interval5TurnedOn) {
+    availableIntervals.push(5);
+  }
+  if (interval6TurnedOn) {
+    availableIntervals.push(6);
+  }
+  if (interval7TurnedOn) {
+    availableIntervals.push(7);
+  }
+  if (interval8TurnedOn) {
+    availableIntervals.push(8);
+  }
+  if (interval9TurnedOn) {
+    availableIntervals.push(9);
+  }
+  if (interval10TurnedOn) {
+    availableIntervals.push(10);
+  }
+  if (interval11TurnedOn) {
+    availableIntervals.push(11);
+  }
+  if (interval12TurnedOn) {
+    availableIntervals.push(12);
+  }
+  if (interval13TurnedOn) {
+    availableIntervals.push(13);
+  }
+  if (interval14TurnedOn) {
+    availableIntervals.push(14);
+  }
+  if (availableIntervals.length > 0)
+  {
+    return availableIntervals[Math.floor(Math.random() * availableIntervals.length)];
+  }
+}
+
+//test btn
+document.getElementById('test').addEventListener('click', () => {
+  playNoteIntervalWithDelayAndMode();
+});
 
 // Pedal button
 pedalButton.addEventListener('click', () => {
@@ -65,6 +178,135 @@ pedalButton.addEventListener('click', () => {
     notes.forEach(note => note.audio.pause());
   }
 });
+
+// Interval buttons
+interval1Button.addEventListener('click', () => {
+  interval1TurnedOn = !interval1TurnedOn;
+  if (interval1TurnedOn) {
+    interval1Button.classList.add('on');
+  }
+  else {
+    interval1Button.classList.remove('on');
+  }
+});
+interval2Button.addEventListener('click', () => {
+  interval2TurnedOn = !interval2TurnedOn;
+  if (interval2TurnedOn) {
+    interval2Button.classList.add('on');
+  }
+  else {
+    interval2Button.classList.remove('on');
+  }
+});
+interval3Button.addEventListener('click', () => {
+  interval3TurnedOn = !interval3TurnedOn;
+  if (interval3TurnedOn) {
+    interval3Button.classList.add('on');
+  }
+  else {
+    interval3Button.classList.remove('on');
+  }
+});
+interval4Button.addEventListener('click', () => {
+  interval4TurnedOn = !interval4TurnedOn;
+  if (interval4TurnedOn) {
+    interval4Button.classList.add('on');
+  }
+  else {
+    interval4Button.classList.remove('on');
+  }
+});
+interval5Button.addEventListener('click', () => {
+  interval5TurnedOn = !interval5TurnedOn;
+  if (interval5TurnedOn) {
+    interval5Button.classList.add('on');
+  }
+  else {
+    interval5Button.classList.remove('on');
+  }
+});
+interval6Button.addEventListener('click', () => {
+  interval6TurnedOn = !interval6TurnedOn;
+  if (interval6TurnedOn) {
+    interval6Button.classList.add('on');
+  }
+  else {
+    interval6Button.classList.remove('on');
+  }
+});
+interval7Button.addEventListener('click', () => {
+  interval7TurnedOn = !interval7TurnedOn;
+  if (interval7TurnedOn) {
+    interval7Button.classList.add('on');
+  }
+  else {
+    interval7Button.classList.remove('on');
+  }
+});
+interval8Button.addEventListener('click', () => {
+  interval8TurnedOn = !interval8TurnedOn;
+  if (interval8TurnedOn) {
+    interval8Button.classList.add('on');
+  }
+  else {
+    interval8Button.classList.remove('on');
+  }
+});
+interval9Button.addEventListener('click', () => {
+  interval9TurnedOn = !interval9TurnedOn;
+  if (interval9TurnedOn) {
+    interval9Button.classList.add('on');
+  }
+  else {
+    interval9Button.classList.remove('on');
+  }
+});
+interval10Button.addEventListener('click', () => {
+  interval10TurnedOn = !interval10TurnedOn;
+  if (interval10TurnedOn) {
+    interval10Button.classList.add('on');
+  }
+  else {
+    interval10Button.classList.remove('on');
+  }
+});
+interval11Button.addEventListener('click', () => {
+  interval11TurnedOn = !interval11TurnedOn;
+  if (interval11TurnedOn) {
+    interval11Button.classList.add('on');
+  }
+  else {
+    interval11Button.classList.remove('on');
+  }
+});
+interval12Button.addEventListener('click', () => {
+  interval12TurnedOn = !interval12TurnedOn;
+  if (interval12TurnedOn) {
+    interval12Button.classList.add('on');
+  }
+  else {
+    interval12Button.classList.remove('on');
+  }
+});
+interval13Button.addEventListener('click', () => {
+  interval13TurnedOn = !interval13TurnedOn;
+  if (interval13TurnedOn) {
+    interval13Button.classList.add('on');
+  }
+  else {
+    interval13Button.classList.remove('on');
+  }
+});
+interval14Button.addEventListener('click', () => {
+  interval14TurnedOn = !interval14TurnedOn;
+  if (interval14TurnedOn) {
+    interval14Button.classList.add('on');
+  }
+  else {
+    interval14Button.classList.remove('on');
+  }
+});
+
 
 // General mouse down/up events
 document.addEventListener('mousedown', function(event) {
